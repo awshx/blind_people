9b2c2aee25aa1b9e36fc0d0a5ee73f7e06d9ee67
JurojinKun <ccommunay@gmail.com>
2019-10-01 11:27:47 +0200

    Création base de données

---
+++ database.sql
@@ -0,0 +1,8 @@
+create database if not exists database_logo; --créer une database si elle n'existe pas
+use database_logo; -- on utilise cette databse
+
+drop table if exists logo; -- on supprime la table utilisateurs de cette database si elle existe
+create table if not exists logo ( -- on creee la table utilisateurs si elle n'existe pas
+	id smallint unsigned not null auto_increment, -- id est la primary key qui s'incremente automatiquement
+	primary key (id)
+);
