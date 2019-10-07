b067da09b0464fd6ab6a9819c720ee31370a19d9
JurojinKun <ccommunay@gmail.com>
2019-10-07 19:38:55 +0200

    Modif/Suppressions

--- database.sql
+++ database.sql
@@ -5,7 +5,14 @@ drop table if exists logo; -- on supprime la table logo de cette database si ell
 create table if not exists logo ( -- on creee la table logo si elle n'existe pas
 	id smallint unsigned not null auto_increment, -- id est la primary key qui s'incremente automatiquement
 	nom_image varchar(255) not null,
-	donnees_binaires longblob not null,
+	lien varchar(255) not null,
 	couleur varchar(40),
 	primary key (id)
 );
+
+/*load data local infile '/netappgfc_vol1/annuaire/annuaire_cc/fusion.csv' -- on transfere les donnees du fichier .csv dans la table Annuaire de cette database
+into table Annuaire
+fields terminated by '\t'
+lines terminated by '\n'
+ignore 1 lines
+(nom, prenom, portable, telephone, fax, multitelephone, poste);*/
