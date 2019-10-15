efc31f7f023ec0d3deea307df489c2eac73f21a6
Boris <boris.dutaud@isen.yncrea.fr>
2019-10-15 14:48:40 +0200

    saturation

--- rgbtohsv.py
+++ rgbtohsv.py
@@ -5,6 +5,7 @@ image = cv2.imread('crab.bmp')
 frame = cv2.cvtColor(image, cv.COLOR_BGR2HSV)
 
 h = frame[:,:,0]
+s = frame[:,:,1]
 
 rouge = 0
 jaune = 0
@@ -13,37 +14,37 @@ cyan = 0
 bleu = 0
 magenta = 0
 
-for i in h:
-	for j in i:
-		if j == 0:
-			continue
-		if j>329/2 or j<30/2:
-			rouge += 1
-		if j>29/2 and j<90/2:
-			jaune += 1
-		if j>89/2 and j<150/2:
-			vert += 1
-		if j>149/2 and j<210/2:
-			cyan += 1
-		if j>209/2 and j<270/2:
-			bleu += 1
-		if j>269/2 and j<330/2:
-			magenta += 1
+for index, element in enumerate(h):
+    for index2, element2  in enumerate(hi):
+        if s[index][index2] < 100:
+            continue
+        if element2>329/2 or element2<30/2:
+            rouge += 1
+        if element2>29/2 and element2<90/2:
+            jaune += 1
+        if element2>89/2 and element2<150/2:
+            vert += 1
+        if element2>149/2 and element2<210/2:
+            cyan += 1
+        if element2>209/2 and element2<270/2:
+            bleu += 1
+        if element2>269/2 and element2<330/2:
+            magenta += 1
 
 m = np.argmax((rouge, jaune, vert, cyan, bleu, magenta))
 
 if m == 0:
-	print("rouge")
+    print("rouge")
 if m == 1:
-	print("jaune")
+    print("jaune")
 if m == 2:
-	print("vert")
+    print("vert")
 if m == 3:
-	print("cyan")
+    print("cyan")
 if m == 4:
-	print("bleu")
+    print("bleu")
 if m == 5:
-	print("magenta")
+    print("magenta")
 
 cv2.imshow("crab",image)
 cv2.waitkey(0)
