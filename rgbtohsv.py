b21f37da4bd3c481622114735a52782e16560aef
isen <mathieu.aoukar@isen.yncrea.fr>
2019-10-15 15:10:19 +0200

    modification mathieu

--- rgbtohsv.py
+++ rgbtohsv.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('crab.bmp')
-frame = cv2.cvtColor(image, cv.COLOR_BGR2HSV)
+image = cv2.imread('carrefour.jpg')
+frame = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 h = frame[:,:,0]
 s = frame[:,:,1]
@@ -15,21 +15,22 @@ bleu = 0
 magenta = 0
 
 for index, element in enumerate(h):
-    for index2, element2  in enumerate(hi):
-        if s[index][index2] < 100:
-            continue
-        if element2>329/2 or element2<30/2:
-            rouge += 1
-        if element2>29/2 and element2<90/2:
-            jaune += 1
-        if element2>89/2 and element2<150/2:
-            vert += 1
-        if element2>149/2 and element2<210/2:
-            cyan += 1
-        if element2>209/2 and element2<270/2:
-            bleu += 1
-        if element2>269/2 and element2<330/2:
-            magenta += 1
+	for index2, element2  in enumerate(element):
+		
+		if s[index][index2] < 100:
+			continue
+		if element2>(329/2) or element2<(30/2):
+			rouge += 1
+		if element2>29/2 and element2<90/2:
+			jaune += 1
+		if element2>89/2 and element2<150/2:
+			vert += 1
+		if element2>149/2 and element2<210/2:
+			cyan += 1
+		if element2>209/2 and element2<270/2:
+			bleu += 1
+		if element2>269/2 and element2<330/2:
+			magenta += 1
 
 m = np.argmax((rouge, jaune, vert, cyan, bleu, magenta))
 
@@ -47,4 +48,5 @@ if m == 5:
     print("magenta")
 
 cv2.imshow("crab",image)
-cv2.waitkey(0)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
