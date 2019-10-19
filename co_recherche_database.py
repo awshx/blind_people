b993c1f089090b02dca5daa91a64f2e7e34e1b18
JurojinKun <ccommunay@gmail.com>
2019-10-19 23:24:03 +0200

    Modififcations pour la sauvegarde des résultats de la recherche

--- co_recherche_database.py
+++ co_recherche_database.py
@@ -1,9 +1,16 @@
 import mysql.connector 
 
 test = "orange"
+i = 0
+tab = [0]*10
 
 baseDeDonnees = mysql.connector.connect(host="localhost",user="root",password="root", database="database_logo")
 curseur = baseDeDonnees.cursor()
-curseur.execute("SELECT * FROM logo WHERE couleur_dominante LIKE '%" + test + "%'")
+curseur.execute("SELECT lien FROM logo WHERE couleur_dominante LIKE '%" + test + "%'")
 for ligne in curseur.fetchall():
-	print(ligne)
+	tab[i] = ligne
+	i += 1
+
+print(tab[0])
+
+baseDeDonnees.close()
