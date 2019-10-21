7cdd8c1562cec3783117b2aa1ff660eb6b48bfcd
isen <mathieu.aoukar@isen.yncrea.fr>
2019-10-21 14:58:31 +0200

    pour clem le rageux

--- version1.py
+++ version1.py
@@ -1,9 +1,49 @@
-import cv2
 import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import math
 
-img = cv2.imread('ntm.png')
+img = cv2.imread('b.jpg')
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+if img is None:
+    print('Could not open the image:')
+    exit(0)
 
-cv2.imshow("image",hsv)
+height = int(np.size(img, 0)/2)
+width = int(np.size(img, 1)/2)
+croppedImg = img[0:0+height, width:int(np.size(img, 1))]
+ntm = croppedImg.copy()
+
+gray = cv2.cvtColor(croppedImg,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,150,255)
+lines = cv2.HoughLines(edges,rho=1,theta=np.pi/180,threshold=130)
+
+_, contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+toto = 0
+
+for cnt in contours:
+
+	perimetre = int(cv2.arcLength(cnt,True))
+
+	if perimetre > 500 and perimetre < 2500:
+		
+		toto += 1
+
+		if toto <= 1:
+
+			approx = cv2.approxPolyDP(cnt,0.01*perimetre,True)
+			
+			if len(cnt) >= 4 and len(cnt) < 250:
+				cv2.drawContours(croppedImg,[cnt],-1,(0,255,0),2)
+
+				(x, y, w, h) = cv2.boundingRect(approx)
+				print('x = ' + str(x) + ' | x+h = ' + str(x+h) + ' | y = ' + str(y) + ' | y+w = ' + str(y+w))
+				newimg = ntm[x:x+h, y:y+w]
+				cv2.imshow("newimg",newimg)
+
+		# vert 120 exterieru 0 | resultat * 25
+
+cv2.imshow("croppedImg",croppedImg)
+cv2.imshow("edges", edges)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
--- watershed.py
+++ watershed.py
@@ -1,44 +1,37 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+import math
 
-img = cv2.imread('a.jpg')
+img = cv2.imread('b.jpg')
+
+if img is None:
+    print('Could not open the image:')
+    exit(0)
+
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
 height = int(np.size(img, 0)/2)
-width = int(np.size(img, 1)/3)
-croppedImg = thresh[0:0+height, 0:width*3]
+width = int(np.size(img, 1)/2)
+croppedImg = thresh[0:0+height, width:int(np.size(img, 1))]
 
-_, contours, _ = cv2.findContours(croppedImg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+_, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 
 for cnt in contours:
 
 	perimetre = int(cv2.arcLength(cnt,True))
-
+	
 	if perimetre > 500 and perimetre < 1000:
 		
-		if len(cnt) >= 4 and len(cnt) < 50:
-		
-			mask = np.zeros_like(img)
-
-			approx = cv2.approxPolyDP(cnt,0.01*perimetre,True)
-			cv2.drawContours(img,[cnt],-1,(0,255,0),2)
-			
-			print(len(cnt))
+		approx = cv2.approxPolyDP(cnt,0.01*perimetre,True)
+		cv2.drawContours(img,[cnt],-1,(0,255,0),2)
+		lines = cv2.HoughLines(croppedImg,rho=1,theta=np.pi/180,threshold=130)
 
 		'''
-
-		perimetre  565
-perimetre  526
-perimetre  802
-perimetre  568
-perimetre  662
-
-
-		if len(cnt) == 4:
-			
+		if len(cnt) >= 4 and len(cnt) < 50:			
 			(x, y, w, h) = cv2.boundingRect(approx)
 			ratio = w / float(h)
 			
@@ -49,11 +42,10 @@ perimetre  662
 				shape = "rectangle"
 				print(shape)
 		'''
-
 	#cv2.putText(img, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 	
-
-cv2.imshow("image",img)
-#cv2.imshow("Threshold", thresh)
+cv2.imshow("image", img)
+cv2.imshow("gray",gray)
+cv2.imshow("Threshold", thresh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
