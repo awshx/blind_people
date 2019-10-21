0f039138abb5252532e8f2223db4fc7b6bf08d91
JurojinKun <ccommunay@gmail.com>
2019-10-21 16:39:38 +0200

    Détection formes

---
+++ detection_forme.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pts_communs as pc
+import cv2
+from matplotlib import pyplot as plt
+import math
+
+img = cv2.imread('./Images_rue/71810630_2435283783192683_6445654989602816_n.jpg')
+
+if img is None:
+    print('Could not open the image:')
+    exit(0)
+
+croppedImg = pc.cropImageLeft(img)
+
+gray = cv2.cvtColor(croppedImg,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,150,255)
+#lines = cv2.HoughLines(edges,rho=1,theta=np.pi/180,threshold=130)
+
+_, contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+num = 0
+
+for cnt in contours:
+
+	perimetre = int(cv2.arcLength(cnt,True))
+
+	if perimetre > 500 and perimetre < 2500:
+
+		approx = cv2.approxPolyDP(cnt,0.01*perimetre,True)
+		
+		if len(cnt) >= 4 and len(cnt) < 500:
+
+			cv2.drawContours(croppedImg,[cnt],-1,(0,255,0),2)
+
+			(x, y, w, h) = cv2.boundingRect(cnt)
+			newimg = croppedImg[y:y+w, x:x+h]
+
+			cv2.imshow("newimg",newimg)
+
+cv2.imshow("croppedImg",croppedImg)
+cv2.imshow("edges", edges)
+cv2.waitKey(10000)
+cv2.destroyAllWindows()
