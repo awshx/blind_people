d63480be44996e185aa7325076b34a9841b59eb9
JurojinKun <ccommunay@gmail.com>
2019-11-12 16:35:20 +0100

    Mise en place de la détection de formes sur les 2 côtés de l'image

--- detection_forme.py
+++ detection_forme.py
@@ -6,52 +6,82 @@ import math
 
 def image():
 	
-	image = './Images_rue/72330137_399179944080045_6899828685730217984_n.jpg'
+	image = './Images_rue/Image1.jpg'
 	return image
 
 def detect_shape():
 
 	img = cv2.imread(image())
+	num = 0
+	tab_formes = []
 
 	if img is None:
 		print('Could not open the image:')
     		exit(0)
 
-	croppedImg = pc.cropImageRight(img)
+	croppedImgRight = pc.cropImageRight(img)
+	croppedImgLeft = pc.cropImageLeft(img)
 
-	gray = cv2.cvtColor(croppedImg,cv2.COLOR_BGR2GRAY)
-	edges = cv2.Canny(gray,150,255)
+	grayRight = cv2.cvtColor(croppedImgRight,cv2.COLOR_BGR2GRAY)
+	edgesRight = cv2.Canny(grayRight,150,255)
 	#lines = cv2.HoughLines(edges,rho=1,theta=np.pi/180,threshold=130)
 
-	_, contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	_, contoursRight, _ = cv2.findContours(edgesRight, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-	num = 0
-	tab_formes = []
+	for cntRight in contoursRight:
 
-	for cnt in contours:
+		perimetreRight = int(cv2.arcLength(cntRight,True))
 
-		perimetre = int(cv2.arcLength(cnt,True))
+		if perimetreRight > 500 and perimetreRight < 2500:
 
-		if perimetre > 500 and perimetre < 2500:
-
-			approx = cv2.approxPolyDP(cnt,0.01*perimetre,True)
+			approxRight = cv2.approxPolyDP(cntRight,0.01*perimetreRight,True)
 		
-			if len(cnt) >= 4 and len(cnt) < 250:
+			if len(cntRight) >= 4 and len(cntRight) < 250:
 
 				#cv2.drawContours(croppedImg,[cnt],-1,(0,255,0),2)
 
-				(x, y, w, h) = cv2.boundingRect(cnt)
-				newimg = croppedImg[y:y+w, x:x+h]
-				heightCropped = int(np.size(newimg, 0))
-				widthCropped = int(np.size(newimg, 1))
+				(x, y, w, h) = cv2.boundingRect(cntRight)
+				newimgRight = croppedImgRight[y:y+w, x:x+h]
+				heightCroppedRight = int(np.size(newimgRight, 0))
+				widthCroppedRight = int(np.size(newimgRight, 1))
 			
 							
-				if (heightCropped/widthCropped) < 1.2 and (heightCropped/widthCropped) >= 0.8:
+				if (heightCroppedRight/widthCroppedRight) < 1.2 and (heightCroppedRight/widthCroppedRight) >= 0.8:
 
 					forme = './Images_detection_formes/forme' + str(num) + '.jpg'	
-					cv2.imwrite(forme , newimg)
+					cv2.imwrite(forme , newimgRight)
 					tab_formes.append(forme)
                         		num += 1
+	
+	grayLeft = cv2.cvtColor(croppedImgLeft,cv2.COLOR_BGR2GRAY)
+        edgesLeft = cv2.Canny(grayLeft,150,255)
+        #lines = cv2.HoughLines(edges,rho=1,theta=np.pi/180,threshold=130)
+
+        _, contoursLeft, _ = cv2.findContours(edgesLeft, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        for cntLeft in contoursLeft:
+
+                perimetreLeft = int(cv2.arcLength(cntLeft,True))
+
+                if perimetreLeft > 500 and perimetreLeft < 2500:
+
+                        approxLeft = cv2.approxPolyDP(cntLeft,0.01*perimetreLeft,True)
+
+                        if len(cntLeft) >= 4 and len(cntLeft) < 250:
+
+                                #cv2.drawContours(croppedImg,[cnt],-1,(0,255,0),2)
+
+                                (x, y, w, h) = cv2.boundingRect(cntLeft)
+                                newimgLeft = croppedImgLeft[y:y+w, x:x+h]
+                                heightCroppedLeft = int(np.size(newimgLeft, 0))
+                                widthCroppedLeft = int(np.size(newimgLeft, 1))
+
+				if (heightCroppedLeft/widthCroppedLeft) < 1.2 and (heightCroppedLeft/widthCroppedLeft) >= 0.8:
+
+                                        forme = './Images_detection_formes/forme' + str(num) + '.jpg'
+                                        cv2.imwrite(forme , newimgLeft)
+                                        tab_formes.append(forme)
+                                        num += 1
 
 	return tab_formes
 
@@ -64,44 +94,76 @@ def coordonnees_centrale_logo():
 		print('Could not open the image:')
     		exit(0)
 
-	croppedImg = pc.cropImageRight(img)
+	croppedImgLeft = pc.cropImageLeft(img)
+	croppedImgRight = pc.cropImageRight(img)	
+
 	yInitiale = 0
 	xInitiale = int(np.size(img, 1)/3)*2
 
-	gray = cv2.cvtColor(croppedImg,cv2.COLOR_BGR2GRAY)
-	edges = cv2.Canny(gray,150,255)
+	grayLeft = cv2.cvtColor(croppedImgLeft,cv2.COLOR_BGR2GRAY)
+	edgesLeft = cv2.Canny(grayLeft,150,255)
 	#lines = cv2.HoughLines(edges,rho=1,theta=np.pi/180,threshold=130)
 
-	_, contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	_, contoursLeft, _ = cv2.findContours(edgesLeft, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-	num = 0
-	tab_formes = []
+	for cntLeft in contoursLeft:
 
-	for cnt in contours:
+		perimetreLeft = int(cv2.arcLength(cntLeft,True))
 
-		perimetre = int(cv2.arcLength(cnt,True))
+		if perimetreLeft > 500 and perimetreLeft < 2500:
 
-		if perimetre > 500 and perimetre < 2500:
-
-			approx = cv2.approxPolyDP(cnt,0.01*perimetre,True)
+			approxLeft = cv2.approxPolyDP(cntLeft,0.01*perimetreLeft,True)
 		
-			if len(cnt) >= 4 and len(cnt) < 250:
+			if len(cntLeft) >= 4 and len(cntLeft) < 250:
 
 				#cv2.drawContours(croppedImg,[cnt],-1,(0,255,0),2)
 
-				(x, y, w, h) = cv2.boundingRect(cnt)
-				newimg = croppedImg[y:y+w, x:x+h]
-				heightCropped = int(np.size(newimg, 0))
-				widthCropped = int(np.size(newimg, 1))
+				(x, y, w, h) = cv2.boundingRect(cntLeft)
+				newimgLeft = croppedImgLeft[y:y+w, x:x+h]
+				heightCroppedLeft = int(np.size(newimgLeft, 0))
+				widthCroppedLeft = int(np.size(newimgLeft, 1))
 			
 							
-				if (heightCropped/widthCropped) < 1.2 and (heightCropped/widthCropped) >= 0.8:
+				if (heightCroppedLeft/widthCroppedLeft) < 1.2 and (heightCroppedLeft/widthCroppedLeft) >= 0.8:
 					coordo_centrale = []
-					coordo_X = xInitiale+x+(widthCropped/2)
-					coordo_Y = yInitiale+y+(heightCropped/2)
-					#cv2.circle(img, (coordo_X, coordo_Y), 7, (0,0,255), -1)
+					coordo_X = xInitiale-(x+(widthCroppedLeft/2)+400)
+					coordo_Y = yInitiale+y+(heightCroppedLeft/2)
+					cv2.circle(img, (coordo_X, coordo_Y), 7, (0,0,255), -1)
 					cv2.imwrite('./Images_detection_formes/coordoCentrale.jpg', img)
 					coordo_centrale.append(coordo_X)
 					coordo_centrale.append(coordo_Y)
+	
+	grayRight = cv2.cvtColor(croppedImgRight,cv2.COLOR_BGR2GRAY)
+        edgesRight = cv2.Canny(grayRight,150,255)
+        #lines = cv2.HoughLines(edges,rho=1,theta=np.pi/180,threshold=130)
 
+        _, contoursRight, _ = cv2.findContours(edgesRight, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        for cntRight in contoursRight:
+
+                perimetreRight = int(cv2.arcLength(cntRight,True))
+
+                if perimetreRight > 500 and perimetreRight < 2500:
+
+                        approxRight = cv2.approxPolyDP(cntRight,0.01*perimetreRight,True)
+
+                        if len(cntRight) >= 4 and len(cntRight) < 250:
+
+                                #cv2.drawContours(croppedImg,[cnt],-1,(0,255,0),2)
+
+                                (x, y, w, h) = cv2.boundingRect(cntRight)
+                                newimgRight = croppedImgRight[y:y+w, x:x+h]
+                                heightCroppedRight = int(np.size(newimgRight, 0))
+                                widthCroppedRight = int(np.size(newimgRight, 1))
+
+				if (heightCroppedRight/widthCroppedRight) < 1.2 and (heightCroppedRight/widthCroppedRight) >= 0.8:
+                                        coordo_centrale = []
+                                        coordo_X = xInitiale+x+(widthCroppedRight/2)
+                                        coordo_Y = yInitiale+y+(heightCroppedRight/2)
+                                        cv2.circle(img, (coordo_X, coordo_Y), 7, (0,0,255), -1)
+                                        cv2.imwrite('./Images_detection_formes/coordoCentrale.jpg', img)
+                                        coordo_centrale.append(coordo_X)
+                                        coordo_centrale.append(coordo_Y)
+
+				
 	return coordo_centrale
