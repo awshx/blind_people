e67c45f134dd093de72c77d9a83a13c8fc752b09
JurojinKun <ccommunay@gmail.com>
2019-11-18 17:24:22 +0100

    Imh maj

--- ihm.py
+++ ihm.py
@@ -4,6 +4,7 @@ from tkinter import *
 from tkinter.filedialog import *
 
 def checkPicture():
+
 	filename  = askopenfilename(title="Ouvrir votre image", filetypes=[('all files','.*')])
 	fichier = open(filename, "r")
 	content = fichier.read()
@@ -15,28 +16,35 @@ def checkPicture():
 	fenetre3.mainloop()
 
 def route_trottoirs():
-	print('route_trottoirs')
+	
+	print('Détection routes, trottoirs,...')
 	fenetre2 = Tk()
-	label = Label(fenetre2, text="Lancement programme: detection route_trottoirs")
+	label = Label(fenetre2, text="Lancement programme: détection route/trottoir")
 	label.pack(side=TOP, padx=25, pady=10)
 
-	Button(fenetre2, text ='find picture', command=checkPicture).pack(padx=25, pady=25)
+	Button(fenetre2, text ='Picture', command=checkPicture).pack(padx=25, pady=25)
 
 	fenetre2.mainloop()
 
 def enseignes():
-	print('enseignes')
-
-fenetre = Tk()
-
-label = Label(fenetre, text="Blind People !")
-label.pack(side=TOP, padx=25, pady=10)
-
-lf = LabelFrame(fenetre, padx=25, pady=25)
-lf.pack(fill="both", expand="yes")
-
-Button(lf, text ='routes / trottoirs', command=route_trottoirs).pack(side=LEFT, padx=25, pady=25)
-Button(lf, text ='enseignes / logos', command=enseignes).pack(side=RIGHT, padx=25, pady=25)
+	
+	print('Reconnaissance logo sur une image')
 
 
-fenetre.mainloop()
+def menu_principal():
+
+	fenetre = Tk()
+
+	label = Label(fenetre, text="Blind People !")
+	label.pack(side=TOP, padx=25, pady=10)
+
+	lf = LabelFrame(fenetre, padx=25, pady=25)
+	lf.pack(fill="both", expand="yes")
+
+	Button(lf, text ='Détection routes/trottoirs', command=route_trottoirs).pack(side=LEFT, padx=25, pady=25)
+	Button(lf, text ='Reconnaissance logo', command=enseignes).pack(side=RIGHT, padx=25, pady=25)
+
+
+	fenetre.mainloop()
+
+menu_principal()
